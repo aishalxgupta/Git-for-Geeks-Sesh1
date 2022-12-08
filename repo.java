1660e9be483d2de2a1b7d86a875e3ddaa1360577
ANUJ-1406 <120120406+ANUJ-1406@users.noreply.github.com>
2022-12-08 21:56:12 +0530

    Create repo.java

---
+++ repo.java
@@ -0,0 +1,6 @@
+public class repo {
+    public static void main(String[] args) {
+      
+      System.out.print{" ANUJ AGGARWAL\n B22088\n CSE\n I can sleep for 15 hrs straight.ㄟ( ▔, ▔ )ㄏ"};
+    }
+}
