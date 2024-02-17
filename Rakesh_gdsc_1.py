0588f53667ed2b76ae960f52c4d4cc2b91f2c3dc
Rakesh11314311 <rakesh11314311@gmail.com>
2024-02-17 20:23:21 +0530

    added a html and css file and modified python code

--- Rakesh_gdsc_1.py
+++ Rakesh_gdsc_1.py
@@ -1,4 +1,4 @@
-print('Name : Rakesh')
+print('\nName : Rakesh')
 print('Roll Number : B22121')
 print('Branch : Computer science and engineering')
 print('Funfact about me : The only kannada speaker in my class.')
